acb21f005ec6a6b2cf59a9751f971305b98efebd
ABIR H. KHANDOKAR <116947694+AKH570@users.noreply.github.com>
2023-02-07 08:26:28 +0600

    this is 2nd commit

--- Laptop/views.py
+++ Laptop/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def laptop_master(request):
-    return HttpResponse ("Welcome to my Laptop Zone")
+    return render (request,'laptop/laptop.html')
--- Mobile/views.py
+++ Mobile/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mobile_master(request):
-    return HttpResponse ("Welcome to my Mobile Zone")
+    return render (request,"mobile/mobile.html")
